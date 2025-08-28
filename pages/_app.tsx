95f875388686876ab0584526da2da7dc1ef78d0a
alex zheng <zhengduoyi12@gmail.com>
2025-08-28 15:42:04 +1000

    style:import Roboto style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/global.css';
 import type { AppProps } from 'next/app';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+  variable: '--font-primary',
+  display: 'swap',
+});
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <div className={roboto.variable}>
+    <Component {...pageProps} />
+  </div>;
 }
