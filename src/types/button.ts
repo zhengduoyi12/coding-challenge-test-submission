f2b3299868e0d2c5e9bb31230147bc32a8033bac
alex zheng <zhengduoyi12@gmail.com>
2025-08-28 18:24:29 +1000

    feat:button type

--- src/types/button.ts
+++ src/types/button.ts
@@ -1,4 +1,4 @@
 import { ButtonHTMLAttributes } from 'react';
 
-export type ButtonType = any;
+export type ButtonType = ButtonHTMLAttributes<HTMLButtonElement>["type"];
 export type ButtonVariant = 'primary' | 'secondary';
